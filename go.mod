7db31366179a79e2835aea7fd2b7cbe1c2a67258
John Wood <j@jdtw.us>
2021-11-19 21:10:19 -0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jdtw/commit
+
+go 1.17
+
+require (
+	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/time/rate"
+	"gopkg.in/yaml.v2"
+)
+
+var port = flag.Int("port", 8080, "listening port")
+
+func main() {
+	flag.Parse()
+	addr := fmt.Sprint(":", *port)
+	log.Printf("listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, newHandler()))
+}
+
+type commitment struct {
+	Commit string `yaml:"commit"`
+	Digest string `yaml:"digest"`
+}
+
+func commit(msg string) (*commitment, error) {
+	entropy := make([]byte, 16)
+	if _, err := rand.Read(entropy); err != nil {
+		return nil, err
+	}
+	msg = fmt.Sprintf("%s %s", msg, hex.EncodeToString(entropy))
+	digest := sha256.Sum256([]byte(msg))
+	return &commitment{
+		Commit: msg,
+		Digest: hex.EncodeToString(digest[:]),
+	}, nil
+}
+
+func newHandler() http.Handler {
+	srv := http.NewServeMux()
+	limiter := rate.NewLimiter(rate.Every(200*time.Millisecond), 5)
+	srv.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		if r.Method != "POST" {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		if !limiter.Allow() {
+			http.Error(w, "Are you like, trying to mine bitcoin or something? Or maybe somebody else is...", http.StatusTooManyRequests)
+			return
+		}
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			internalError(w, "failed to read body: %v", err)
+			return
+		}
+		if len(body) == 0 {
+			http.Error(w, "No message...", http.StatusBadRequest)
+			return
+		}
+		response, err := commit(string(body))
+		w.Header().Set("Content-Type", "text/yaml")
+		yaml.NewEncoder(w).Encode(response)
+	})
+	return srv
+}
+
+func internalError(w http.ResponseWriter, format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Print(msg)
+	http.Error(w, msg, http.StatusInternalServerError)
+}
